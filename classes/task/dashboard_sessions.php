3fe81919f19df1ff172248d615f3f6ebb42ca952
Sikeze <mipozarski@alumnos.uai.c>
2017-01-16 14:08:19 -0300

    task final fix

--- classes/task/dashboard_sessions.php
+++ classes/task/dashboard_sessions.php
@@ -22,7 +22,7 @@
 * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 namespace local_dashboard\task;
-class dashboard_sessions extends \core\task\scheduled_tasks {
+class dashboard_sessions extends \core\task\scheduled_task {
 
 	public function get_name() {
 		return get_string('sessiontask', 'local_dashboard');
